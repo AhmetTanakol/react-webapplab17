7525d4e00b7ef1fe62cc92d2aa5e522fef24dcef
Ahmet Tanakol <ahmettanakol@gmail.com>
2018-01-16 13:59:08 +0100

    updated seed app

--- src/index.js
+++ src/index.js
@@ -7,15 +7,51 @@ import registerServiceWorker from './registerServiceWorker';
 import schema from './schema.json';
 import uischema from './uischema.json';
 import { initJsonFormsStore } from '@jsonforms/core';
-import { materialize } from '@jsonforms/material-renderers';
+
+const translations = {
+  'en-US': {
+    firstname: 'First Name',
+    lastname: 'Last Name',
+    streetnumber: 'Street Number',
+    postalcode: 'Postal Code',
+    age: 'Age',
+    dateofbirth: 'Date Of Birth',
+    dateandtimeofparty: 'Date and Time of Party',
+    timeofarrival: 'Time of Arrival',
+    height: 'Height',
+    gender: 'Gender',
+    rating: 'Rating',
+    committer: 'Committer',
+    weight: 'Weight',
+    drivingskill: 'Driving Skill',
+    street: 'Street',
+    city: 'City'
+  },
+  'de-DE': {
+    firstname: 'Vorname',
+    lastname: 'Name',
+    streetnumber: 'Hausnummer',
+    postalcode: 'Postleitzahl',
+    age: 'Alter',
+    dateofbirth: 'Geburtsdatum',
+    dateandtimeofparty: 'Datum und Uhrzeit der Party',
+    timeofarrival: 'Ankunftszeit',
+    height: 'Höhe',
+    gender: 'Geschlecht',
+    rating: 'Bewertung',
+    committer: 'Vermittler',
+    weight: 'Gewicht',
+    drivingskill: 'Fahrkünste',
+    street: 'Strasse',
+    city: 'Stadt'
+  }
+};
 
 const store = initJsonFormsStore({
   firstName: "Max",
   personalData: {}
-}, schema, uischema);
+}, schema, uischema, translations);
 
-materialize();
-console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <App />
